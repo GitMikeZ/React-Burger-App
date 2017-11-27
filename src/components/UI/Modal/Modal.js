0700f46d02f7e43d742bab0fdb3cf4f1c0607af8
Mike Z <minqi03@gmail.com>
2017-11-26 23:29:32 -0800

    Modal Animation

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -4,10 +4,13 @@ import classes from './Modal.css';
 
 const modal = (props) => (
 
-	<div className={classes.modal}>
+	<div className={classes.modal}
+		style={{
+			transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+			opacity: props.show ? '1':'0'
+		}}>
 		{props.children}
 	</div>
-
 );
 
 export default modal;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -23,7 +23,8 @@ class BurgerBuilder extends Component {
 			meat: 0
 		},
 		totalPrice: 4,
-		purchasable: false
+		purchasable: false,
+		purchasing: false
 	}
 
 	updatePurchaseState(ingredients) {
@@ -69,6 +70,10 @@ class BurgerBuilder extends Component {
 		this.updatePurchaseState(updatedIngredients);
 	}
 
+	purchaseHandler = () =>  {
+		this.setState({purchasing: true});
+	}
+
 	render() {
 		const disabledInfo = {
 			...this.state.ingredients
@@ -78,7 +83,7 @@ class BurgerBuilder extends Component {
 		}
 		return (
 			<Aux>
-				<Modal>
+				<Modal show={this.state.purchasing}>
 					<OrderSummary ingredients={this.state.ingredients}/>
 				</Modal>
 				<Burger ingredients={this.state.ingredients} />
@@ -86,6 +91,7 @@ class BurgerBuilder extends Component {
 					ingredientAdded={this.addIngredientHandler}
 					ingredientRemoved={this.removeIngredientHandler}
 					purchasable={this.state.purchaseable}
+					ordered={this.purchaseHandler}
 					price={this.state.totalPrice} />
 			</Aux>
 		);

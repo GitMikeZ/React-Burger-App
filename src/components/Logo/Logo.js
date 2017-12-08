2f5e881cef70503589e0abf65170da9fe72bbdfe
Mike Z <minqi03@gmail.com>
2017-12-08 00:00:41 -0800

    Responsive DrawerToggle

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import burgerLogo from '../../assets/images/burger-logo.png';
 import classes from './Logo.css';
 
 const logo = (props) => (
-	<div classes={classes.Logo}>
+	<div classes={classes.Logo} style={{height: props.height}}>
 		<img src={burgerLogo} alt="MyBurger" />
 	</div>
 );
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,11 +3,12 @@ import classes from './Toolbar.css';
 
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems'
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle'
 
 const toolbar = (props) => (
 	<header className={classes.Toolbar}>
-		<div>MENU</div>
-		<nav>
+		<DrawerToggle clicked={props.drawerToggleClicked}/>
+		<nav className={classes.DesktopOnly}>
 			<NavigationItems/>
 		</nav>
 	</header>
